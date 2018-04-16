b5338e9f427e167192925634ad7736baf14d4a2b
Johnny Mayer and Kayla Ondracek <kayla@>
2018-04-16 09:31:31 -0700

    add categories list and views/controller

--- ToDoList/Controllers/CategoriesController.cs
+++ ToDoList/Controllers/CategoriesController.cs
@@ -3,15 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-
+using ToDoList.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace ToDoList.Controllers
 {
     public class CategoriesController : Controller
     {
+
+        private ToDoListDbContext _db;
+        public CategoriesController(ToDoListDbContext db)
+        {
+            _db = db;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            List<Category> model = _db.Categories.ToList();
+            return View(model);
         }
     }
 }
